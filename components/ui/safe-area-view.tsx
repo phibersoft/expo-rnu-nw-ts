2272ad216b688ccb2ef16752f8b98f5de8acf6b5
phibersoft <spirnet@hotmail.com>
2025-06-30 23:09:07 +0300

    impl: safe-area-view common component

---
+++ components/ui/safe-area-view.tsx
@@ -0,0 +1,42 @@
+import {
+  Platform,
+  SafeAreaView as OriginalSafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  ViewProps,
+} from "react-native";
+import { FC } from "react";
+
+type SafeAreaViewProps = ViewProps & {
+  insideScrollView?: boolean;
+};
+
+export const SafeAreaView: FC<SafeAreaViewProps> = ({
+  children,
+  style,
+  insideScrollView = true,
+  ...props
+}) => {
+  return (
+    <OriginalSafeAreaView
+      style={StyleSheet.compose(style, styles.androidSafeArea)}
+      {...props}
+    >
+      {insideScrollView ? (
+        <ScrollView style={{ flex: 1 }}>{children}</ScrollView>
+      ) : (
+        children
+      )}
+    </OriginalSafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  androidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingBottom: 90, // tabbar
+    backgroundColor: "#fff",
+  },
+});
